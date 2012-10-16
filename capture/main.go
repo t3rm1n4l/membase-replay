8ed74cce8ef0d2a89299eb6bc18d9704120bee12
Sarath Lakshman <slakshman@zynga.com>
2012-10-16 18:24:34 +0530

    Initial commit for membase request capture tool

---
+++ capture/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/akrennmair/gopcap"
+)
+
+func parseCommand(msg string) (cmd string, flag bool) {
+	flag = false
+	switch {
+	case strings.HasPrefix(msg, "set"):
+		fallthrough
+	case strings.HasPrefix(msg, "cas"):
+		fallthrough
+	case strings.HasPrefix(msg, "add"):
+		flag = true
+		cmd = strings.Join(strings.SplitN(msg, " ", 6)[:5], " ")
+
+	case strings.HasPrefix(msg, "get"):
+		fallthrough
+	case strings.HasPrefix(msg, "getl"):
+		fallthrough
+	case strings.HasPrefix(msg, "delete"):
+		flag = true
+		cmd = strings.Join(strings.SplitN(msg, " ", 3)[:2], " ")
+	}
+	return
+}
+
+func main() {
+	var device *string = flag.String("i", "", "interface")
+	var infile *string = flag.String("r", "", "pcap file")
+	var snaplen *int = flag.Int("s", 65535, "snaplen")
+	var outfile *string = flag.String("o", "outfile.cap", "capture file")
+	var h *pcap.Pcap
+	var err error
+	expr := "tcp dst port 11211"
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "usage: %s [ -i interface | -r file.pcap ] [ -o filename.cap ]\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	flag.Parse()
+
+	switch {
+
+	case *device != "":
+		h, err = pcap.Openlive(*device, int32(*snaplen), true, 0)
+	case *infile != "":
+		h, err = pcap.Openoffline(*infile)
+	default:
+		devs, err := pcap.Findalldevs()
+		if err != nil || len(devs) == 0 {
+			fmt.Fprintf(os.Stderr, "Couldn't find any devices: %s\n", err)
+			os.Exit(1)
+		}
+		h, err = pcap.Openlive(devs[0].Name, int32(*snaplen), true, 0)
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not initialize pcap (%s)\n", err)
+		os.Exit(1)
+	}
+
+	defer h.Close()
+
+	if expr != "" {
+		ferr := h.Setfilter(expr)
+		if ferr != nil {
+			fmt.Fprintf(os.Stderr, "Unable to set filter :%s (%s)\n", expr, ferr)
+			os.Exit(1)
+		}
+	}
+
+	f, ferr := os.Create(*outfile)
+	if ferr != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create file:%s (%s)\n", *outfile, ferr)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	stream := bufio.NewWriter(f)
+	defer stream.Flush()
+
+	for pkt := h.Next(); pkt != nil; pkt = h.Next() {
+		pkt.Decode()
+		if len(pkt.Headers) != 0 {
+			for _, msg := range strings.Split(string(pkt.Payload), "\r\n") {
+
+				if cmd, ok := parseCommand(msg); ok {
+					fmt.Fprintf(stream, "%d,%s:%d,%s\n", pkt.Time.UnixNano(), pkt.IP.SrcAddr(), pkt.TCP.SrcPort, cmd)
+					stream.Flush()
+				}
+			}
+		}
+	}
+}
---
+++ replay/main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+
+var wait sync.WaitGroup
+var connections map[string]chan string
+var count int
+
+func genRequest(cmd string) string {
+	params := strings.Split(cmd, " ")
+
+	var key string
+	var flag int
+	var exp int
+	var length int
+	ret := ""
+
+	switch params[0] {
+	case "set":
+		fallthrough
+	case "cas":
+		fallthrough
+	case "add":
+		key = params[1]
+		flag, _ = strconv.Atoi(params[2])
+		exp, _ = strconv.Atoi(params[3])
+		length, _ = strconv.Atoi(params[4])
+		ret = fmt.Sprintf("set %s %d %d %d 0001:\n%0*s\n", key, flag, exp, length, length)
+
+	case "get":
+		fallthrough
+	case "getl":
+		key = params[1]
+		ret = fmt.Sprintf("get %s\n", key)
+
+	case "delete":
+		key = params[1]
+		ret = fmt.Sprintf("delete %s\n", key)
+	}
+	return ret
+
+}
+
+func respReader(conn *net.Conn) {
+	reader := bufio.NewReader(*conn)
+	defer wait.Done()
+
+	for {
+		_, err := reader.ReadString('\n')
+		if err != nil {
+			return
+		}
+
+	}
+}
+
+func handleConnection(server string, ch chan string) {
+	conn, err := net.Dial("tcp", server)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect server:%s (%s)\n", server, err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+	defer wait.Done()
+
+	wait.Add(1)
+	go respReader(&conn)
+
+	for {
+		msg, ok := <-ch
+		if !ok {
+			return
+		}
+		conn.Write([]byte(msg))
+	}
+
+}
+
+func main() {
+	var capfile *string = flag.String("c", "", "Capture data file")
+	var server *string = flag.String("h", "127.0.0.1:11211", "Server address")
+	connections = make(map[string]chan string)
+	count = 0
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage %s: -c data.cap -h server:port\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	flag.Parse()
+
+	f, err := os.Open(*capfile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to open file:%s (%s)\n", *capfile, err)
+		os.Exit(1)
+	}
+	defer f.Close()
+	reader := csv.NewReader(f)
+	replayStartTime := time.Now()
+	var actualStartTime time.Time
+	firstEntry := false
+
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+
+		v, _ := strconv.Atoi(record[0])
+		recordTime := time.Unix(0, int64(v))
+
+		if !firstEntry {
+			firstEntry = true
+			actualStartTime = recordTime
+		}
+
+		toSleep := recordTime.Sub(actualStartTime) - time.Now().Sub(replayStartTime)
+		time.Sleep(toSleep)
+		ch := connections[record[1]]
+		if ch == nil {
+			ch = make(chan string, 1)
+			connections[record[1]] = ch
+			wait.Add(1)
+			go handleConnection(*server, ch)
+		}
+		ch <- genRequest(record[2])
+	}
+
+	for _, v := range connections {
+		close(v)
+	}
+	wait.Wait()
+
+}
